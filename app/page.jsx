1d1a928f198fe9b613a2b737b9fdacb7ce33c7ba
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-01-25 19:27:34 +0530

    span added

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Promts</span>
       </h1>
     </section>
   )
