8bced81ec2235045819803a6976df432f18ae217
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-01-26 09:58:32 +0530

    -Change isUserLoggedInn to false to design and view output if user isn;t logged in yet. -Make a useState for providers, needed for next-auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,9 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
