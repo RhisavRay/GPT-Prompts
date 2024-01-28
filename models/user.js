12876e9bccb7e64286dc979e6eb6eae8657315c7
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-01-28 23:55:49 +0530

    Attributes for email added

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from "mongoose"
+
+// To create a new schema, the "newSchema" keyword is used
+
+const userSchema = newSchema({
+    
+    // Now here, one by one all atributes  of our User will be added to this schema.
+
+    email: {
+        type: String, //  This means that it can only store strings in MongoDB
+        unique: [true, "Email already exists!"], //  The true parameter makes sure that every email must be different for new users.
+        required: [true, "Email is required!"] //  The second parameter is a validation message if the field is empty.
+    },
+
+    // Now from here, all other needed  attributes are going to follow the same pattern as above.
+})
