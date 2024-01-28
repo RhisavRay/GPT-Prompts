0dff6807ed68c527f6ecd958d1d18da8a358bd26
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-01-28 21:05:50 +0530

    Google provider added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,2 +1,11 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth/next"
 import GoogleProvider from 'next-auth/providers/google'
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: "",
+            clientSecret: ""
+        })
+    ]
+})
